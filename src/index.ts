95117a87cfe96b3addbd525196cb05ab6dc075bc
Sobhan <s.jahanmard@gmail.com>
2022-11-19 10:22:28 +0330

    feat- add screen shot script as a comment

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import puppeteer from "puppeteer";
   const resultsSelector = ".gsc-results .gs-title";
   await page.waitForSelector(resultsSelector);
   // Extract the results from the page.
+
   const links = await page.evaluate((resultsSelector: any) => {
     return [...Array.from(document.querySelectorAll(resultsSelector))].map(
       (anchor: any) => {
@@ -26,3 +27,14 @@ import puppeteer from "puppeteer";
   console.log(links.join("\n\n"));
   await browser.close();
 })();
+
+// const url =
+//   "https://montmovie.co/category/4/%D8%B3%D8%B1%DB%8C%D8%A7%D9%84-%D8%B3%D8%B1%DB%8C%D8%A7%D9%84-%D8%AE%D8%A7%D8%B1%D8%AC%DB%8C/";
+// async function run() {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto(url);
+//   await page.screenshot({ path: "screenshot.png" });
+//   browser.close();
+// }
+// run();
